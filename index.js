185050d37844452424ecadd14f1a6b8374cb31d8
Robert Krahn <robert.krahn@gmail.com>
2014-03-25 01:25:28 -0700

    switching to use lively.MaoduleSystem for basic module logic

--- index.js
+++ index.js
@@ -3,244 +3,99 @@ var path = require("path");
 var fs = require("fs");
 var async = require("async");
 
-var root = '';
-
 function log(/*args*/) {
   var args = Array.prototype.slice.call(arguments);
   args[0] = '[lv-module-loader] ' + args[0];
   console.log.apply(console, args);
 }
 
-function resolve(moduleOrName) {
-  var name, module
-  if (typeof moduleOrName === 'string') {
-    name = moduleOrName; module = lvModule(name);
-  } else {
-    name = moduleOrName.namespaceIdentifier; module = moduleOrName;
-  }
-
-  var ext = '.js';
-  var fileName = name.replace(/\./g, path.sep) + ext;
-  var fullPath = path.join(root, fileName)
-
-  module.__cachedUri = fullPath;
-
-  return function(next) { next(null, module); }
+function resolve(url) {
+    return function(next) {
+        next(null, url.replace(/^file:\/\//, ''));
+    }
 }
 
-function readFromDisk(module, next) {
-  log("readFromDisk %s", module.uri());
-  fs.readFile(module.uri(), function(err, content) {
-    module.__source = content;
-    next(err, module);
+function readFromDisk(fn, next) {
+  log("readFromDisk %s", fn);
+  fs.readFile(fn, function(err, content) {
+    next(err, fn, content);
   });
 }
 
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
-var moduleRegistry = {};
-
-function lvModule(mName) {
-  if (moduleRegistry[mName]) return moduleRegistry[mName];
-
-  var newModule = {
-
-    callbacks: [],
-    namespaceIdentifier: mName,
-    pendingRequirements: [],
-    dependendModules: [],
-    requires: null,
-    wasDefined: false,
-    _isLoaded: false,
-
-    uri: function() {
-      return this.__cachedUri;
-    },
-
-    addRequiredModule: function (requiredModule) {
-        if (requiredModule.isLoaded()) return;
-        this.pendingRequirements.push(requiredModule);
-        requiredModule.addDependendModule(this);
-    },
-
-    removeRequiredModule: function (requiredModule) {
-      log("%s was loaded", requiredModule);
-      if (this.pendingRequirements && this.pendingRequirements.indexOf(requiredModule) === -1) {
-        console.error('requiredModule not there');
-      }
-      this.pendingRequirements = this.pendingRequirements.filter(function(req) {
-        return req !== requiredModule; });
-      if (!this.hasPendingRequirements()) { this.load(); }
-    },
-
-    hasPendingRequirements: function () {
-      if (this.pendingRequirements && this.pendingRequirements.length > 0) return true;
-      // if (this.requiredLibs && this.requiredLibs.any(function(libSpec) { return !libSpec.loadTest(); })) return true;
-      return false;
-    },
-
-    loadRequirementsFirst: function () {
-      this.pendingRequirements && this.pendingRequirements.forEach(function(ea) { ea.load(); });
-      // this.requiredLibs && this.requiredLibs.invoke('load');
-    },
-
-    addDependendModule: function (depModule) {
-      this.dependendModules.push(depModule);
-    },
-
-    informDependendModules: function () {
-      var deps = this.dependendModules;
-      this.dependendModules = [];
-      deps.forEach(function(ea) { ea.removeRequiredModule(this) }, this);
-    },
-
-    addOnloadCallback: function (cb, idx) {
-      if (typeof idx === 'number') this.callbacks.splice(idx,0,cb);
-      else this.callbacks.push(cb);
-    },
-
-    runOnloadCallbacks: function () {
-        var cb, id = this.namespaceIdentifier;
-        while ((cb = this.callbacks.shift())) {
-          try { cb(); } catch(e) {
-            console.error('%s runOnloadCallbacks: %s: %s', cb.name, e);
-            throw e;
-          }
-        };
-    },
-
-    load: function(loadSync) {
-      if (loadSync) throw new Error('sync loading not yet supported');
-      if (this.isLoaded()) {
-          log("%s loaded, running callbacks...", this, this.callbacks);
-          this.runOnloadCallbacks();
-          return;
-      }
-
-      if (this.wasDefined && !this.hasPendingRequirements()) {
-          log("%s not yet loaded but has no requirements, running callbacks...", this);
-          this.runOnloadCallbacks();
-          this.informDependendModules();
-          return;
-      }
-
-      if (this.isLoading() || this.wasDefined) {
-          log("%s not yet loaded, loading %s first", this, this.pendingRequirements);
-          this.loadRequirementsFirst();
-          return;
-      }
-
-      load(this.namespaceIdentifier);
-
-    },
-
-    isLoaded: function() {
-      return this._isLoaded;
-    },
-
-    isLoading: function () {
-      if (this.isLoaded()) return false;
-      // if (this.uri().include('anonymous')) return true;
-      return !!this.wasDefined;
-    },
-
-    activate: function() {},
-    deactivate: function() {},
-
-    toString: function() {
-      return util.format("lvModule(%s)", this.namespaceIdentifier);
-    }
-
-  };
-
-  newModule.requires = lvRequire.bind(newModule);
-
-  return moduleRegistry[mName] = newModule;
+function wrapCode(fn, content, next) {
+  log("wrapCode %s", fn);
+  var header = 'var require = lively.require;\n'
+             + 'var module = lively.module;\n';
+  next(null, fn, header + content);
 }
 
-function lvRequire(/*deps*/) {
-  var args = Array.prototype.slice.call(arguments);
-  var thisModule = this;
-  var dependencyNames = args.length === 1 && Array.isArray(args[0]) ?
-    args[0] : args;
-
-  var requiredModules = dependencyNames.map(lvModule);
-
-  requiredModules.forEach(thisModule.addRequiredModule.bind(thisModule));
-
-  function moduleExecute(code) {
-      var debugCode = code;
-       // pass in own module name for nested requirements
-      code = code.bind(null, thisModule);
-       // run code with namespace modules as additional parameters
-      function codeWrapper() {
-          try {
-              thisModule.activate();
-              code.apply(this, requiredModules);
-              thisModule._isLoaded = true;
-          } catch(e) {
-              console.error;(e, debugCode);
-          } finally {
-              thisModule.deactivate();
-          }
-      }
-      thisModule.addOnloadCallback(codeWrapper, 0/*add as first callback*/);
-      // wasDefined: module body and module requirements encountered but
-      // body not necessarily executed or requirements loaded
-      thisModule.wasDefined = true;
-      thisModule.load();
-  }
-
-  return {toRun: moduleExecute};
-
-}
-
-// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
-function wrapModule(module, next) {
-  log("wrapModule %s", module);
-  var header = 'var require = lvRequire;\n'
-             + 'var module = lvModule;\n';
-  module.__wrappedSource = header + module.__source;
-  next(null, module);
-}
-
-function runModule(m, next) {
-  log("runModule %s", m);
+function runCode(fn, source, next) {
+  log("runCode %s", fn);
   var err;
   try {
-    eval(m.__wrappedSource);
+    eval(source);
   } catch (e) {
-    console.error("Error when loading %s: ", m, e.stack || e);
+    console.error("Error when loading %s: ", fn, e.stack || e);
     err = e;
   }
-  next(err, m);
+  next(err, fn);
 }
 
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-function setRoot(dir) { root = dir; }
+var loader = {
 
-function load(moduleName, thenDo) {
-  log('about to intialize loading %s', moduleName);
-  async.waterfall([
-    resolve(moduleName),
-    readFromDisk,
-    wrapModule,
-    runModule
-  ], function(err, m) {
-    log('initialized loading %s', m);
-    if (!thenDo) return;
-    if (m.isLoaded()) thenDo(null);
-    else m.addOnloadCallback(thenDo.bind(null,null/*err*/));
-  });
+    loadJs: function (url, onLoadCb, loadSync, okToUseCache, cacheQuery) {
+      async.waterfall([
+        resolve(url),
+        readFromDisk,
+        wrapCode,
+        runCode
+      ], function(err) {
+          if (err) { console.error("failed loading %s:\n", url, err); return; }
+          log('loaded %s%s', url);
+          onLoadCb && onLoadCb();
+      });
+    }
 }
 
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+var started = false;
+function start(options, thenDo) {
+    if (started) { thenDo && thenDo(null); return; }
+    var options = options ? util._extend(defaultOptions, options) : defaultOptions;
+    require('./lib/bootstrap')(defaultOptions);
+    started = true;
+    thenDo && thenDo(null);
+}
+
+var baseURL = 'file://' + process.env.LIVELY;
+
+var defaultOptions = {
+    loader: loader,
+    rootPath: baseURL,
+    location: baseURL,
+    locationDirectory: baseURL,
+    get codeBase() { return defaultOptions.rootPath + 'core/'; },
+    nodeJSURL: 'http://localhost:9001',
+    bootstrapFiles: [
+        'core/lively/Migration.js',
+        // 'core/lively/JSON.js',
+        'core/lively/lang/Object.js',
+        'core/lively/lang/Function.js',
+        'core/lively/lang/String.js',
+        'core/lively/lang/Array.js',
+        'core/lively/lang/Number.js',
+        'core/lively/lang/Date.js',
+        // 'core/lively/lang/Worker.js',
+        // 'core/lively/lang/LocalStorage.js',
+        // 'core/lively/defaultconfig.js',
+        'core/lively/Base.js',
+        'core/lively/ModuleSystem.js']
+}
 
 module.exports = {
-  setRoot: setRoot,
-  load: load,
-  modules: moduleRegistry,
-  module: lvModule
+    defaultOptions: defaultOptions,
+    start: start
 }
--- tests/test.js
+++ tests/test.js
@@ -5,7 +5,7 @@ var tests = {
 
   setUp: function (callback) {
     global.livelyLoaderTests = { loadedModules: [] }
-    callback();
+    livelyLoader.start({}, callback);
   },
 
   tearDown: function (callback) {
@@ -13,19 +13,28 @@ var tests = {
     callback();
   },
 
+  testIfItWorksAtAll: function(test) {
+    test.ok(!!Object.extend, 'lively extensions not loaded?');
+    test.ok(!!lively.Config, 'lively objects not loaded?');
+    test.ok(!!lively.Module, 'lively objects not loaded 2?');
+
+    var m = lively.module('foo.bar');
+    // var m = new lively.Module(Global, 'foo.bar');
+    test.equals(m.uri(), livelyLoader.defaultOptions.codeBase + 'foo/bar.js');
+    test.done();
+  },
+
   testSimpleModuleLoad: function(test) {
-    livelyLoader.setRoot(path.join(__dirname, 'test-modules'));
-    livelyLoader.load('a', function(err) {
-      test.ok(!err, String(err));
+    lively.Config.codeBase = 'file://' + path.join(__dirname, 'test-modules') + '/';
+    lively.require('a').toRun(function() {
     	test.deepEqual(livelyLoaderTests.loadedModules, ['a']);
     	test.done();
-    });
+    })
   },
 
   testLoadModuleWithDependency: function(test) {
-    livelyLoader.setRoot(path.join(__dirname, 'test-modules'));
-    livelyLoader.load('b', function(err) {
-      test.ok(!err, String(err));
+    lively.Config.codeBase = 'file://' + path.join(__dirname, 'test-modules') + '/';
+    lively.require('b').toRun(function() {
     	test.deepEqual(livelyLoaderTests.loadedModules, ['dir1.c', 'b']);
     	test.done();
     });
