0297acb88cbe97fbc3f040b59f2be9e0748ec570
Robert Krahn <robert.krahn@gmail.com>
2016-06-01 23:59:36 -0700

    cleanup, 0.3

--- index.js
+++ index.js
@@ -3,53 +3,39 @@
 var util       = require("util");
 var path       = require("path");
 var fs         = require("fs");
-var async      = require("async");
 var livelyLang = require("lively.lang");
 var debug      = false;
 
 function log(/*args*/) {
-    if (!debug) return;
-    var args = Array.prototype.slice.call(arguments);
-    args[0] = '[lv-module-loader] ' + args[0];
-    console.log.apply(console, args);
+  if (!debug) return;
+  var args = Array.prototype.slice.call(arguments);
+  args[0] = '[lv-module-loader] ' + args[0];
+  console.log.apply(console, args);
 }
 
-function resolve(url) {
-    return function(next) { next(null, url.replace(/^file:\/\//, '')); };
-}
+function resolve(url) { return url.replace(/^file:\/\//, ''); }
 
-function readFromDisk(fn, next) {
-    log("readFromDisk %s", fn);
-    fs.readFile(fn, function(err, content) { next(err, fn, content); });
+function readFromDisk(fn) {
+  log("readFromDisk %s", fn);
+  return fs.readFileSync(fn);
 }
 
 function wrapCode(fn, content, next) {
     log("wrapCode %s", fn);
-    var code = ";(function livelyCodeInNodejs(require, module, nodejs) {\n"
-            + content
-            + "\n})(lively.require, lively.module, {require: require, module: module});";
-    next(null, fn, code);
+    return ";(function livelyCodeInNodejs(require, module, nodejs) {\n"
+         + content
+         + "\n})(lively.require, lively.module, {require: require, module: module});";
 }
 
 function runCode(fn, source, next) {
     log("runCode %s", fn);
-    var err;
     try {
-        source += '\n//@ sourceURL=' + fn + '\n';
-        eval(source);
-    } catch (e) {
-        console.error("Error when loading %s: ", fn, e.stack || e);
-        err = e;
+      source += '\n//# sourceURL=' + fn + '\n';
+      eval(source);
+    } catch (err) {
+      console.error("Error when loading %s: ", fn, err.stack || err);
+      throw err;
     }
-    next(err, fn);
-}
-
-function parseJSON(fn, json, next) {
-    var jso;
-    try {
-        jso = JSON.parse(json);
-    } catch (e) { next(e, fn, null); return; }
-    next(null, fn, jso);
 }
 
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
@@ -57,12 +43,12 @@ function parseJSON(fn, json, next) {
 var loadState = {};
 
 function ensureLoadState(url) {
-    return loadState[url] || (loadState[url] = {
-        isLoading: false,
-        isLoaded: false,
-        error: null,
-        callbacks: []
-    });
+  return loadState[url] || (loadState[url] = {
+    isLoading: false,
+    isLoaded: false,
+    error: null,
+    callbacks: []
+  });
 }
 
 function isLoading(url) { return !!ensureLoadState(url).isLoading; }
@@ -72,118 +58,108 @@ function isLoaded(url) { return !!ensureLoadState(url).isLoaded; }
 function hasError(url) { return !!ensureLoadState(url).error; }
 
 function signalIsLoaded(url, optContent) {
-    var st = ensureLoadState(url);
-    if (typeof optContent !== "undefined") st.result = optContent;
-    st.isLoading = false;
-    st.isLoaded = true;
-    while (st.callbacks[0]) st.callbacks.shift().call(global, null, optContent);
+  var st = ensureLoadState(url);
+  if (typeof optContent !== "undefined") st.result = optContent;
+  st.isLoading = false;
+  st.isLoaded = true;
+  while (st.callbacks[0]) st.callbacks.shift().call(global, null, optContent);
 }
 
 function signalIsLoading(url) {
-    var st = ensureLoadState(url);
-    st.isLoading = true;
-    st.isLoaded = false;
+  var st = ensureLoadState(url);
+  st.isLoading = true;
+  st.isLoaded = false;
 }
 
 function signalError(url, err) {
-    var st = ensureLoadState(url);
-    st.isLoading = false;
-    st.isLoaded = false;
-    st.error = err;
+  var st = ensureLoadState(url);
+  st.isLoading = false;
+  st.isLoaded = false;
+  st.error = err;
     while (st.callbacks[0]) st.callbacks.shift().call(global, err);
 }
 
 function load(url, onLoadCb, contentProcessor) {
-    if (hasError(url)) {
-        log("trying to load %s but previous load attempt threw error!");
-        return;
-    }
-    if (isLoaded(url)) { onLoadCb && onLoadCb.call(global, ensureLoadState(url).result); return; }
-    addCallback(url, onLoadCb);
-    if (isLoading(url)) return;
-    signalIsLoading(url);
-    async.waterfall([
-        resolve(url),
-        readFromDisk,
-        contentProcessor
-    ], function(err, fn, result) {
-        log('loaded %s', url);
-        if (err) {
-            log("failed loading %s:\n", url, err);
-            signalError(url, err);
-        } else signalIsLoaded(url, result);
-    });
-
+  if (hasError(url)) {
+    log("trying to load %s but previous load attempt threw error!");
+    return;
+  }
+  if (isLoaded(url)) { onLoadCb && onLoadCb.call(global, ensureLoadState(url).result); return; }
+  addCallback(url, onLoadCb);
+  if (isLoading(url)) return;
+  signalIsLoading(url);
+  try {
+    var fn = resolve(url),
+        content = readFromDisk(fn),
+        result = contentProcessor(fn, content);
+  } catch (err) {
+    log("failed loading %s:\n", url, err);
+    signalError(url, err);
+    return;
+  }
+  log('loaded %s', url);
+  signalIsLoaded(url, result);
 }
 
 function addCallback(url, callback) {
-    callback && ensureLoadState(url).callbacks.push(callback);
+  callback && ensureLoadState(url).callbacks.push(callback);
 }
 
 var loader = {
-
-    loadJs: function (url, onLoadCb, loadSync, okToUseCache, cacheQuery) {
-        load(url, onLoadCb, function(fileName, content, next) {
-            async.waterfall([
-                wrapCode.bind(null, fileName, content),
-                runCode
-            ], next);
-        });
-    },
-
-    loadJSON: function (url, onLoadCb, beSync) {
-        load(url, onLoadCb, function(fileName, content, next) {
-            parseJSON(fileName, content, next);
-        });
-    }
+  loadJs: function (url, onLoadCb, loadSync, okToUseCache, cacheQuery) {
+    load(url, onLoadCb, function(fileName, content) { return runCode(fileName, wrapCode(fileName, content)); });
+  },
+  loadJSON: function (url, onLoadCb, beSync) {
+    load(url, onLoadCb, function(fileName, content) { return JSON.parse(content); });
+  }
 }
 
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
 var started = false;
 function start(options, thenDo) {
-    if (started) { thenDo && thenDo(null); return; }
+  if (started) { thenDo && thenDo(null); return; }
 
-    var lvDir = options.livelyDirectory || process.env.WORKSPACE_LK || process.env.LIVELY;
-    if (!lvDir) throw new Error("Cannot find the Lively base direcrory. Use the option livelyDirectory or the environment variable LIVELY to set it.");
-    if (!lvDir.match(/\/$/)) lvDir += "/";
-    options.livelyDirectory = lvDir;
+  var lvDir = options.livelyDirectory || process.env.WORKSPACE_LK || process.env.LIVELY;
+  if (!lvDir) throw new Error("Cannot find the Lively base direcrory. Use the option livelyDirectory or the environment variable LIVELY to set it.");
+  if (!lvDir.match(/\/$/)) lvDir += "/";
+  options.livelyDirectory = lvDir;
 
-    var server, addr;
-    if (options && options.lifeStar) {
-        server = options.lifeStar.getServer();
-        addr = server.address();
+  var server, addr;
+  if (options && options.lifeStar) {
+    server = options.lifeStar.getServer();
+    addr = server.address();
+  }
+
+  var baseURL = 'file://' + lvDir,
+    defaultOptions = {
+      loader: loader,
+      rootPath: baseURL,
+      location: {
+        isNodejs: true,
+        // href: "...",
+        host: addr ? addr.address + ":" + addr.port : null,
+        hostname: addr ? addr.address : null,
+        href: null,
+        origin: null,
+        pathname: null,
+        port: addr ? addr.port : addr,
+        protocol: server ? (server.config.enableSSL ? "https:" : "http:") : null,
+        toString: function() { return baseURL; }
+      },
+      locationDirectory: baseURL,
+      get codeBase() { return defaultOptions.rootPath.replace(/\/?$/, '/') + 'core/'; },
+      nodeJSURL: 'http://localhost:9001'
     }
 
-    var baseURL = 'file://' + lvDir,
-        defaultOptions = {
-            loader: loader,
-            rootPath: baseURL,
-            location: {
-                isNodejs: true,
-                // href: "...",
-                host: addr ? addr.address + ":" + addr.port : null,
-                hostname: addr ? addr.address : null,
-                href: null,
-                origin: null,
-                pathname: null,
-                port: addr ? addr.port : addr,
-                protocol: server ? (server.config.enableSSL ? "https:" : "http:") : null,
-                toString: function() { return baseURL; }
-            },
-            locationDirectory: baseURL,
-            get codeBase() { return defaultOptions.rootPath.replace(/\/?$/, '/') + 'core/'; },
-            nodeJSURL: 'http://localhost:9001'
-        }
+  livelyLang.deprecatedLivelyPatches();
 
-    livelyLang.deprecatedLivelyPatches();
-
-    options = options ? util._extend(defaultOptions, options) : defaultOptions;
-    require('./lib/bootstrap')(defaultOptions, function(err, lively) {
-        if (err) console.error("Error in lively-loader: ", err);
-        started = true;
-        thenDo && thenDo(err, lively);
-    });
+  options = options ? util._extend(defaultOptions, options) : defaultOptions;
+  require('./lib/bootstrap')(defaultOptions, function(err, lively) {
+    if (err) console.error("Error in lively-loader: ", err);
+    started = true;
+    thenDo && thenDo(err, lively);
+  });
 }
 
 module.exports = {
